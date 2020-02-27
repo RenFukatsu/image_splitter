10711fd1932af6ba0bd17bff2b0d3c71728a1d2c
RenFukatsu <fukatsuren88@gmail.com>
2020-02-27 10:24:12 +0900

    update launch

--- launch/image_splitter-launch.py
+++ launch/image_splitter-launch.py
@@ -8,8 +8,7 @@ def generate_launch_description():
             node_executable='image_splitter',
             output='screen',
             node_name='image_splitter',
-            remappings=[('image', 'usb_cam/image_raw')],
-            parameters=['parameters.yaml']
-            # parameters=[{'hz':0.0}, {'save_dir_name':'/home/amsl/colcon_ws/src/human_tracking_using_robots/bagfiles/fox/images/alone1'}]
-        )
+            remappings=[('image', 'your_image_topic')],
+            parameters=[{'hz':0.0, 'save_dir_path':'your_save_dir'}]
+        ),
     ])
